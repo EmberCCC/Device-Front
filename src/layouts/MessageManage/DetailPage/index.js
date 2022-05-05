ea332b88d51c85b20b218baf5c1f5a64d30af340
EmberCCC <1810888456@qq.com>
2022-05-06 02:33:02 +0800

    2022.5.6

--- src/layouts/MessageManage/DetailPage/index.js
+++ src/layouts/MessageManage/DetailPage/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-04-25 22:05:14
- * @LastEditTime: 2022-05-06 02:21:12
+ * @LastEditTime: 2022-05-06 02:32:42
  * @LastEditors: EmberCCC 1810888456@qq.com
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \bl-device-manage-test\src\layouts\MessageManage\DetailPage\index.js
@@ -28,8 +28,8 @@ class DetailPage extends Component {
     }
     render() {
         const { modalVisible ,itemInfo} = this.props.MessageStore
-        let ok = '了解'
-        let cancel = '关闭'
+        let ok = this.props.MessageStore.subFlag ? '同意' : "了解"
+        let cancel = this.props.MessageStore.subFlag ? '拒绝' : "关闭"
         return (
             <div>{modalVisible && <GlobalModal
                 width={1000}
@@ -41,7 +41,6 @@ class DetailPage extends Component {
                         // console.log(itemInfo);
                         let params = {}
                         params.message = '同意'
-                        ok = '同意'
                         params.messageDto = toJS(itemInfo)
                         this.props.MessageStore.agreeFlow(params)
                     }
@@ -52,7 +51,6 @@ class DetailPage extends Component {
                     if (this.props.MessageStore.subFlag) {
                         // console.log(itemInfo);
                         let params = {}
-                        cancel = '拒绝'
                         params.message = '拒绝'
                         params.messageDto = toJS(itemInfo)
                         this.props.MessageStore.refuseFlow(params)
