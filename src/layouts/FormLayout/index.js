24a85a20873f394a676518ed83880d509f944567
EmberCCC <1810888456@qq.com>
2022-08-11 14:20:04 +0800

    小版本

--- src/layouts/FormLayout/index.js
+++ src/layouts/FormLayout/index.js
@@ -2,7 +2,7 @@
  * @Author: EmberCCC 1810888456@qq.com
  * @Date: 2022-07-02 03:21:54
  * @LastEditors: EmberCCC 1810888456@qq.com
- * @LastEditTime: 2022-08-10 20:22:48
+ * @LastEditTime: 2022-08-11 14:19:46
  * @FilePath: \bl-device-manage-test\src\layouts\FormLayout\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -49,11 +49,11 @@ const FormLayout = observer(({ HomeStore, FormStore }) => {
       )
       return;
     }
-    // if (checkArr.length > 0) {
-    //   FormStore.submitDataCheck({ 'formId': firstFormId, 'data': { ...formData, ...nData, ...data }, 'checkFieldIds': checkArr })
-    // } else {
-    //   FormStore.submitData({ 'formId': firstFormId, 'data': { ...formData, ...nData, ...data } })
-    // }
+    if (checkArr.length > 0) {
+      FormStore.submitDataCheck({ 'formId': firstFormId, 'data': { ...formData, ...nData, ...data }, 'checkFieldIds': checkArr })
+    } else {
+      FormStore.submitData({ 'formId': firstFormId, 'data': { ...formData, ...nData, ...data } })
+    }
     console.log(checkArr);
     form.resetFields();
     formList.resetFields();
